7052007d221211d058b05e054385d759b8f6e106
Abdülkadir Güntav <guntav.abdulkadir@gmail.com>
2026-08-11 21:08:20 +0300

    Revert "test(ui): update widget test for onboarding UI"
    This reverts commit 5f0225f29450fa7aa786eca049d608a3c7a99de7.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,12 +11,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:bonfire/main.dart';
 
 void main() {
-  testWidgets('App shows onboarding title', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
-    await tester.pumpAndSettle();
 
-    expect(find.text('Welcome to Bonfire'), findsOneWidget);
-    expect(find.byType(TextField), findsOneWidget);
-    expect(find.text('Choose your class'), findsOneWidget);
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
